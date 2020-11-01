8a9583aa31460b67ff778ea679846fac6c085575
Julio201213 <73481582+Julio201213@users.noreply.github.com>
2020-11-01 17:46:13 -0500

    Add files via upload

---
+++ Ejercicio3/lab3.js
@@ -0,0 +1,36 @@
+//inicio
+//variables
+
+let MamadeJuan;
+let Juan;
+let Alberto;
+let Ana;
+let suma=0;
+
+//leer datos
+
+Juan = Number(prompt('Ingresar Edad de Juan'));
+
+//operacion
+
+Alberto = (Juan *2)/3;
+Ana = (Juan *4)/3;
+MamadeJuan = Juan + Alberto + Ana;
+
+//imprimir
+
+alert(" La edad de Juan es  " + Juan);
+document.write(" La edad de Juan es  " + Juan);
+console.log(" La edad de Juan es " + Juan);
+alert(" La edad de Albertp es  " + Alberto);
+document.write(" La edad de Alberto es  " + Alberto);
+console.log(" La edad de Alberto es " + Alberto);
+alert(" La edad de Ana es  " + Ana);
+document.write(" La edad de Ana es  " + Ana);
+console.log(" La edad de Ana es " + Ana);
+alert(" La edad de la Mama de Juan es  " + MamadeJuan);
+document.write(" La edad de la Mama de Juan es  " + MamadeJuan);
+console.log(" La edad de la Mama de Juan es es " + MamadeJuan);
+
+// Final
+ 
